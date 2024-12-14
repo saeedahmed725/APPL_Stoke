aeec7374378f25c367c9aaecf3c595c7e98133c5
saeedahmed725 <saeedahmedsaeed725@gmail.com>
2024-12-14 23:12:41 +0200

    lasttttttttttttttttttttttttttttttttttt

--- APPL_Stoke_API.py
+++ APPL_Stoke_API.py
@@ -7,8 +7,7 @@ from fastapi import FastAPI
 import uvicorn
 import yfinance as yf
 import tensorflow as tf
-print("TensorFlow version:", tf.__version__)
-print("GPU Available:", tf.config.list_physical_devices('GPU'))
+
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -21,8 +20,8 @@ scaler = MinMaxScaler()
 def fetch_last_60_minutes():
     ticker = yf.Ticker("AAPL")
     data = ticker.history(period="1d", interval="1m")
-    last_60_minutes = data.tail(60)["Close"]  # Fetch last 60 Close prices
-    return last_60_minutes.values
+    last_60_minutes = data.tail(60)
+    return last_60_minutes
 
 # Function to scrape the real-time AAPL price
 def scrape_aapl_data():
@@ -40,22 +39,27 @@ def predict_stock():
 
     # Fetch last 60 Close prices
     last_60_closes = fetch_last_60_minutes()
-
     # Reshape and scale the data
-    close_df = pd.DataFrame(last_60_closes, columns=["Close"])
+    close_df = pd.DataFrame(last_60_closes, columns=['Open','High', 'Low', 'Close', "Volume"])
     scaled_data = scaler.fit_transform(close_df)
-    X_input = scaled_data.reshape(1, 60, 1)  # Reshape for LSTM input
+    X_input = scaled_data.reshape(1, 60, 5) 
 
     # Predict the next Close price
     predicted_price = model.predict(X_input)
-    predicted_price = scaler.inverse_transform([[predicted_price[0][0]]])
+    predicted_price = model.predict(X_input)
+
+    input_for_inverse_transform = np.array([[predicted_price[0][0], 0, 0, 0, 0]])
+
+    predicted_price_inverse = scaler.inverse_transform(input_for_inverse_transform)
+
+
 
     # Fetch the current real-time price
     current_price = scrape_aapl_data()
 
     return {
         "scraped_current_price": current_price,
-        "predicted_price": predicted_price
+        "predicted_price": predicted_price_inverse[0][0]
     }
 
 if __name__ == "__main__":
